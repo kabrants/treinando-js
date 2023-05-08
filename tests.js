42ee3652e5b2ff13dd1dc22e123a38a105d5336f
Kauê <kaueefonseca@gmail.com>
2023-05-08 16:39:37 -0300

    Treinando variável let, o que pode e o que não pode fazer quando utilizar a variável let

---
+++ tests.js
@@ -0,0 +1,5 @@
+console.log('Kauê nasceu em 1997.');
+console.log('Em 2014 Kauê conheceu Tayná.');
+console.log('Kauê casou-se com Tayná em 2025.');
+console.log('Tayná teve 1 filho com Kauê em 2026.');
+console.log('O filho de Kauê se chama Kalleb.');
