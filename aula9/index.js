51e2a8f9610313232b8a5e86b11e8b31ab24b184
Kauê <kaueefonseca@gmail.com>
2023-05-09 20:09:21 -0300

    treinando adição, concatenação, potenciação, resto da divisão, incrementos, decrementos, pós e pré, NaN, parseInt, parseFloat e Number no lugar dos dois anteriores

---
+++ aula9/index.js
@@ -0,0 +1,11 @@
+//COM VAR PODE DECLARAR E REDECLARAR, LET NÃO
+// Dados Primitivos: String, Number, Undefined, Null, Boolean
+const nome = 'Kauê'; // String
+const nome1 = "Abrantes"; // String
+const nome3 = `Abrantes`; // String
+const num1 = 10; // Number
+const num2 = 10.52; // Number com ponto flutuante
+let nomeAluno; // Undefined = não aponta para local nenhuma na memória
+let sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memória
+const aprovado = false; // Boolean = true ou false (lógico)
+
