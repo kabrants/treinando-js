aaac43d745ef1ea5e164ceb2ba7b95c7f622c9ca
Kauê <kaueefonseca@gmail.com>
2023-05-09 10:49:18 -0300

    Aprendendo const(constantes) e que não pode ser alteradas na outra linha como o let pode

---
+++ aula7/index.js
@@ -0,0 +1,9 @@
+// dentro de aspas String = Text | fora de aspas Number = Número
+const primeiroNumero = 5; 
+const segundoNumero = 10;
+const resultado = primeiroNumero * segundoNumero;
+const resultaDuplicado = resultado * 2;
+let resultadoTriplicado = resultado* 3;
+resultadoTriplicado = resultadoTriplicado + 5;
+
+console.log(typeof (primeiroNumero + segundoNumero));
